78aad825236cbde7b85d2e51ab382b9ac979ca6c
HoGaSan <hoga.san@gmail.com>
2017-04-29 17:12:23 +0200

    Data preparation tasks

--- 02-OnTimeFlightPerformanceDataSetDataPreparation.R
+++ 02-OnTimeFlightPerformanceDataSetDataPreparation.R
@@ -12,7 +12,8 @@
 #Downloading the files
 
 method="auto"
-for (i in 1987:2016){
+#Files are available from 1987, but we need them only from 1990
+for (i in 1990:2016){
   for (j in 1:12){
     sourceFile <- paste("On_Time_On_Time_Performance_", i, "_", j, ".zip", sep = "")
     URL <- paste("https://www.transtats.bts.gov/PREZIP/", sourceFile, sep = "")
@@ -31,17 +32,37 @@ for (i in 1987:2016){
   }
 }
 
-##OK##############################
+#setwd("DataSets")
+#setwd("..")
+#getwd()
+
+#Unzipping
+for (i in 1990:2016){
+  for (j in 1:12){
+    #zipped file unzipping
+    zippedFileName <- paste("On_Time_On_Time_Performance_", i, "_", j, ".zip", sep = "")
+    zippedFile <- paste(getwd(), "/DataSets/", zippedFileName, sep = "")
+    print(paste("Unzipping: ",zippedFile,sep=""))
+    #No overwrite, so if the unzip was done, then it's not done again.
+    #Gives warning because of the "readme.html" file, which is there in every zip file.
+    unzip(zippedFile, overwrite = FALSE, exdir = paste(getwd(), "/DataSets", sep = ""))
+  }
+}
+
+#warnings()
 
 
-#files moved to /tmp/flightData
+#variable creation
 
-#unzip the file
-destfile2 <- "/app/finalProject/flightData/On_Time_On_Time_Performance_1987_11.zip"
-unzip(destfile2, exdir = "/app/finalProject/flightData/")
-
-unzippedfile <- "/app/finalProject/flightData/On_Time_On_Time_Performance_1987_10.csv"
-
-system.time(
-On_Time_On_Time_Performance_1987_10 <- data.table(read.csv(unzippedfile, header = TRUE))
-)
+for (i in 1990:2016){ #2016
+  for (j in 1:12){
+    #unzipped file handling
+    unzippedFileName <- paste("On_Time_On_Time_Performance_", i, "_", j, ".csv", sep = "")
+    unzippedFile <- paste(getwd(), "/DataSets/", unzippedFileName, sep = "")
+    variableName <- paste("On_Time_On_Time_Performance_", i, "_", j, sep = "")
+    if (file.exists(unzippedFile) == TRUE){
+      print(variableName)
+      assign(variableName, data.table(read.csv(unzippedFile, header = TRUE)))
+    }
+  }
+}
--- 99 - Additional information.R
+++ 99 - Additional information.R
@@ -3,9 +3,26 @@
 #On Time On Time Performance
 #Number of files: 360
 #Size (zipped): 6,419,851,748 bytes
-#Size (unzipped): 
+#Number of files after 1990: 324
+#Size (zipped): 6,196,385,360 bytes
+#Size (unzipped): 71,146,030,010 bytes
 
-#Wildlife Strike
+##Wildlife Strike
 #Number of files: 1
 #Size (zipped): 44,730,852 bytes
-#Size (unzipped): 
+#Size (unzipped): 193,495,040 bytes
+
+#STRIKE_REPORTS (1990-1999).csv
+  #30082 rows
+  #Size: 21,523,668 bytes
+#STRIKE_REPORTS (2000-2009).csv
+  #69960 rows
+  #Size: 51,833,820 bytes
+#STRIKE_REPORTS (2010-Current).csv
+  #70577 rows
+  #Size: 53,973,874 bytes
+#STRIKE_REPORTS_BASH (1990-Current).csv
+  #8046 rows
+  #Size: 5,412,394 bytes
+
+
